a8b3c69e53ba585af62fc739ee2bc75ae707ff19
mugesh004gk <gkmugesh2004@gmail.com>
2026-03-05 16:45:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app= express()
 app.use(express.json())
 const port = 4000
 const mongoose=require("mongoose")
+
 const URL = "mongodb+srv://gkmugesh2004_db_user:qW88MXliYhfm5qv4@cluster0.evjdyui.mongodb.net/?appName=Cluster0"
 const userRouter=require("./router/userRouter")
 const detailsRouter=require("./router/detailsRouter")
